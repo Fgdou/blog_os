d0e4beb842a8118cc4795dcd14eaf60b5559a05c
Fgdou <fabigoardou@gmail.com>
2026-01-03 18:59:21 +0100

    debug interrupt

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
-#![feature(custom_test_frameworks)]
+#![feature(custom_test_frameworks, abi_x86_interrupt)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
 
 use core::panic::PanicInfo;
 
@@ -42,6 +43,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
@@ -67,4 +69,8 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     unsafe {
         port.write(exit_code as u32);
     }
+}
+
+pub fn init() {
+    interrupts::init_idt();
 }
--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,8 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("Hello world{}", "!");
 
+    blog_os::init();
+
     #[cfg(test)]
     test_main();
 
