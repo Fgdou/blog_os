faef6bc19fa40cb25e7cea1851d443649704d1d2
Fgdou <fabigoardou@gmail.com>
2026-01-04 15:43:55 +0100

    feat heap allocator and tests

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ extern crate alloc;
 
 use core::panic::PanicInfo;
 
-use alloc::boxed::Box;
+use alloc::{boxed::Box, vec::Vec};
+use blog_os::{allocator, memory::{self, BootInfoFrameAllocator}};
 use bootloader::{BootInfo, entry_point};
+use x86_64::VirtAddr;
 
 mod vga_buffer;
 mod serial;
@@ -34,10 +36,15 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     blog_os::init();
 
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe {memory::init(phys_mem_offset)};
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
     #[cfg(test)]
     test_main();
-
-    let x = Box::new(1);
 
     println!("It did not crash!");
 
